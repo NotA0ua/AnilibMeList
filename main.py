548a1778a3c817c8aa2e8c8fa319cffaef9a8387
nota0ua <demiddubrovskylite@gmail.com>
2025-06-14 14:33:48 +0300

    0.1.0

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from bs4 import BeautifulSoup
 
+
 def get_user_input() -> tuple[str, str, str]:
     file_path = input("Enter html path: ")
     print("\n")
     list_name = input("Enter the name of your list: ")
     print("\n")
-    output_type = input("Enter the type of output (MDT, md): ")
+    output_type = input("Enter the type of output (mdt, md): ")
     return file_path, list_name, output_type
+
 
 def read_file(path: str) -> str:
     with open(path, "r", encoding="utf-8") as file:
         return file.read()
+
 
 def get_elements(page: str) -> list[tuple[str, str]]:
     soup = BeautifulSoup(page, "lxml")
@@ -28,8 +31,9 @@ def get_elements(page: str) -> list[tuple[str, str]]:
 
     return anime
 
+
 def generate_md_table(anime: list[tuple[str, str]]) -> str:
-    table =  "| Аниме | Link |\n"
+    table = "| Аниме | Ссылка |\n"
     table += "| ----- | ---- |\n"
     for anim in anime:
         table += f"| {anim[0]} | {anim[1]} |\n"
@@ -41,15 +45,16 @@ def generate_md(anime: list[tuple[str, str]]) -> str:
     output = str()
 
     for anim in anime:
-        output += f"[{anim[0]}]({anim[1]})\n"
+        output += f"- [{anim[0]}]({anim[1]})\n"
 
     return output
 
+
 def create_md_file(list_name: str, output: str) -> None:
-    with open("generated.md", "w") as file:
+    with open(f"{list_name}.md", "w") as file:
         file.write(output)
 
-    print("Successfully created generated.md")
+    print(f"Successfully created {list_name}.md")
 
 
 def main() -> None:
